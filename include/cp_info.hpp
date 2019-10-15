629fbd4525df415885904c045a5dd4c4573724c9
brenfeliped <brenofeliped15@live.com>
2019-10-15 15:32:36 -0300

    Added: definição de tags Constant_pool

--- include/cp_info.hpp
+++ include/cp_info.hpp
@@ -19,6 +19,21 @@ class Cp_info {
 		// colocar aqui a estrutura que vai guardar 
 		// os elementos do constant pool
 	public:
+		static const uint8_t CONSTANT_Class = 7;
+		static const uint8_t CONSTANT_Fieldref = 9;
+		static const uint8_t CONSTANT_Methodref = 10;
+		static const uint8_t CONSTANT_InterfaceMethodref = 11;
+		static const uint8_t CONSTANT_String = 8;
+		static const uint8_t CONSTANT_Integer = 3;
+		static const uint8_t CONSTANT_Float = 4;
+		static const uint8_t CONSTANT_Long = 5;
+		static const uint8_t CONSTANT_Double = 6;
+		static const uint8_t CONSTANT_NameAndType = 12;
+		static const uint8_t CONSTANT_Utf8 = 1;
+		static const uint8_t CONSTANT_MethodHandle = 15;
+		static const uint8_t CONSTANT_MethodType  = 16;
+		static const uint8_t CONSTANT_InvokeDynamic = 18;
+		 
 		u4 getConstantPoolEntry(u2 tag);
 
 };
