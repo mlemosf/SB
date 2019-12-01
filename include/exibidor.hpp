3037e6630e7184694ea96988da5134f0156db670
RenanZX <renangodoimedeiros@gmail.com>
2019-12-01 14:18:42 -0300

    Finalizando documentação doxie do Leitor e exibidor

--- include/exibidor.hpp
+++ include/exibidor.hpp
@@ -15,7 +15,8 @@
 #elif __linux__
   #define CLS "clear"
 #endif
-
+/** Classe do exibidor responsável por exibir os dados a partir de um leitor
+*/
 class Exibidor {
 private:
 	Leitor r;
@@ -23,13 +24,38 @@ private:
 	void ShowField(int s);
 	void ShowMethod(int s);
 public:
+	/** Construtor do exbidor
+	recebe um leitor a ser exibido como parametro
+	@param l leitor a ser exibido
+	*/
 	Exibidor(Leitor l);
+	/** Função de mostrar
+	Imprime todos os dados lidos pelo leitor
+	*/
 	void Mostrar();
+	/** Função de informações Gerais
+	Imprime todos os dados referentes às informações gerais lidas do leitor
+	*/
 	void GeneralInformation();
+	/** Função de Constant Pool
+	Imprime todos os dados referentes ao constant pool lido do leitor
+	*/
 	void ConstantPool();
+	/** Função de Interfaces
+	Imprime todos os dados referentes às interfaces lidas do leitor
+	*/
 	void Interfaces();
+	/** Função de Fields
+	Imprime todos os dados referentes aos campos lidos do leitor
+	*/
 	void Fields();
+	/** Função de Metodos
+	Imprime todos os dados referentes aos metodos lidos do leitor
+	*/
 	void Methods();
+	/** Função de Atributos
+	Imprime todos os dados referentes aos atributos lidos do leitor
+	*/
 	void Attributes();
 };
 
